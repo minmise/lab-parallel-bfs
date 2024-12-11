f04024e18d64038fb4b70a516bdac0dfe17f4fbc
minmise <dokhru@gmail.com>
2024-12-11 19:52:27 +0300

    complete test_time

--- test_time.cpp
+++ test_time.cpp
@@ -21,26 +21,34 @@ int test_time_par(const std::vector<std::vector<int>> &graph) {
     return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 }
 
+void add_edge(std::vector<std::vector<int>> &graph, int v1, int v2) {
+    graph[v1].emplace_back(v2);
+    graph[v2].emplace_back(v1);
+}
+
 int main() {
-    /*const int t = 5;
-    const int size = 500 * 500 * 500;
+    const int t = 5;
+    const int dimensions = 3;
+    const int size_base = 500;
+    int size = 1;
+    for (int i = 0; i < dimensions; ++i) {
+        size *= size_base;
+    }
     int sum_seq = 0;
     int sum_par = 0;
-    for (int i = 0; i < t; ++i) {
-        int *arr = new int[size];
-        gen_test_random(arr, size);
-        int *arr_copy = new int[size];
-        for (int j = 0; j < size; ++j) {
-            arr_copy[j] = arr[j];
+    std::vector<std::vector<int>> graph(size);
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0, cur_diff = 1; j < dimensions && i + cur_diff < size; ++j, cur_diff *= size_base) {
+            add_edge(graph, i, i + cur_diff);
         }
-        int value_seq = test_time_seq(arr, size);
-        int value_par = test_time_par(arr_copy, size);
-        delete[] arr;
-        delete[] arr_copy;
+    }
+    for (int i = 0; i < t; ++i) {
+        int value_seq = test_time_seq(graph);
+        int value_par = test_time_par(graph);
         sum_seq += value_seq;
         sum_par += value_par;
-        std::cout << "test " << i + 1 << " seq time: " << value_seq << " ms; par time: " << value_par << " ms\n";
+        std::cout << "launch " << i + 1 << " seq time: " << value_seq << " ms; par time: " << value_par << " ms\n";
     }
     std::cout << "seq average time: " << sum_seq / t << " ms; par average time: " << sum_par / t << " ms\n";
-    return 0;*/
+    return 0;
 }
