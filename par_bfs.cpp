735e000f0c546b320153117effd0aba99e8f2a92
minmise <dokhru@gmail.com>
2024-12-13 21:53:06 +0300

    parallel is 40% faster

--- par_bfs.cpp
+++ par_bfs.cpp
@@ -13,7 +13,7 @@
 static void bfs(const std::vector<std::vector<int>> &graph, std::vector<int> &dist) {
     int size = graph.size();
     dist[0] = 0;
-    std::vector<std::atomic<int>> used(size);
+    std::vector<std::atomic_int> used(size);
     used[0].store(1);
     parlay::parallel_for(1, size, [&](int i) {
         used[i].store(0);
@@ -23,30 +23,35 @@ static void bfs(const std::vector<std::vector<int>> &graph, std::vector<int> &di
     parlay::sequence<int> degs_pref;
     while (!cur_frontier.empty()) {
         int frontier_size = cur_frontier.size();
-        degs_pref = parlay::tabulate(frontier_size, [&](int i) {
+        /*degs_pref = parlay::tabulate(frontier_size, [&](int i) {
             return static_cast<int>(graph[cur_frontier[i]].size());
+        });*/
+        degs_pref.resize(frontier_size);
+        parlay::parallel_for(0, frontier_size, [&](int i) {
+            degs_pref[i] = graph[cur_frontier[i]].size();
         });
         if (degs_pref.empty()) {
             break;
         }
         parlay::scan_inplace(degs_pref);
+        next_frontier.clear();
         next_frontier.resize(degs_pref.back() + static_cast<int>(graph[cur_frontier.back()].size()), -1);
         parlay::parallel_for(0, frontier_size, [&](int v_ind) {
             int v = cur_frontier[v_ind];
-            parlay::parallel_for(0, graph[v].size(), [&](int u_ind) {
+            //parlay::parallel_for(0, graph[v].size(), [&](int u_ind) {
+            for (int u_ind = 0; u_ind < graph[v].size(); ++u_ind) {
                 int u = graph[v][u_ind];
                 int value = 0;
                 if (used[u].compare_exchange_strong(value, 1)) {
                     dist[u] = dist[v] + 1;
                     next_frontier[degs_pref[v_ind] + u_ind] = u;
                 }
-            }, 10);
+            }
+            //}, 8);
         });
         cur_frontier = parlay::filter(next_frontier, [](int val) {
             return val >= 0;
         });
-        next_frontier.clear();
-        degs_pref.clear();
     }
 }
 
